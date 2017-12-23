4cd79051a1d64c88d46927919bc515eef3231e4c
Lopa Nayak <chinu.nayak13@gmail.com>
2017-12-23 11:43:16 -0700

    Java_inh_Protected_with_comments

--- Animal.java
+++ Animal.java
@@ -1,10 +1,27 @@
-package oOPS_concepts;
+/** Scene 1: parent.food() is protected field.-> 
+ create a parent obj in child class, and trying to access the parent.food() from the 
+ main function of child class gives compile error. 
+ 
+ Reason:  parent object created outside home package has no access to any protected 
+ members in that package - even if that member is its own class member.  
+ 
+ **/
+package oOPS_concepts;// default access modifier means all its members are visible only in the package.
+
+import oOps_concepts_2.Bird;
 
 public class Animal {
 protected void food()
 {
 	System.out.print("solid and not just water");
-	
+}
+
+public static void main(String args[])
+{
+	Animal an2Obj = new Animal();	
+	an2Obj.food();
+	Bird bi = new Bird(); // bi object is invoked in its own class context hence works fine. 
+	bi.food();
 }
 }
 
--- Bird.java
+++ Bird.java
@@ -1,16 +1,35 @@
+/** This exercise shows the true essence of protected access modifier in inheritance relationship. 
+ * Lets create an object of superclass or reference of superclass pointing to subclass 
+ * object in the context of the subclass.
+ * Scene 1: superclass method food() is declared as protected.
+ * sup_obj.food() --> compile time error.
+ * Scene 2: superclass method food() is declared public
+ * sup_obj.food() --> no error
+ * Scene 3: subclass object is created in subclass main method
+ * sub_obj.food() --> compiles file. 
+ * conclusion: Subclass object in subclass main method has access to superclass protected method is a foreign package only because of protected access modifier
+ * superclass object has no access to its own method because it is created outside its home package. 
+ *  **/
+
 package oOps_concepts_2;
+import oOPS_concepts.*;
 
-public class Bird extends oOPS_concepts.Animal {
+public class Bird extends Animal {
 
-	protected void food()
+	/*protected void food()
 	{
 		System.out.print("grains/fruits ");
-	}
+	}*/
 	
 	public static void main(String args[])
 	{
-		oOPS_concepts.Animal an = new Bird();
-		an.food();
-// Compile time error as method food from Animal class is not visible here. Animal.foo that methods are protected
+		
+		Animal an2Obj = new Animal();
+		Animal anRef = new Bird();
+	//	anRef.food(); // compile error
+	//	an2Obj.food(); // compile error
+     
+		Bird bi = new Bird(); // bi object is invoked in its own class context hence works fine. 
+		bi.food();
 	}
 }
